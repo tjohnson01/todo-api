0206f011153e18c1fad0cec6a12cf8e7a723fec8
Travis Johnson <tjohnson01@gmail.com>
2015-10-02 11:19:14 -0600

    test

--- server.js
+++ server.js
@@ -123,15 +123,30 @@ app.post('/todos', function(req, res){
 // DELETE /todos/:id
 app.delete('/todos/:id', function(req, res){
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos, {id: todoId});
+   
+    db.todo.destroy({
+        where :{
+            id: todoId
+        }
+    }).then(function(rowsDeleted){
+        if(rowsDeleted > 0){
+            res.json("Row with ID: " + todoId + " deleted successfully");
+        } else{
+            res.status(404).json("No rows found to delete");
+        }
+    }, function(err){
+        res.status(500).json(err);
+    });
 
-    if(!matchedTodo)
-        res.status(404).json({"error": "no todo found with that ID"});
-    else{
-        //remove element from array
-        todos = _.without(todos, matchedTodo);
-        res.json(todos);
-    }
+    // var matchedTodo = _.findWhere(todos, {id: todoId});
+
+    // if(!matchedTodo)
+    //     res.status(404).json({"error": "no todo found with that ID"});
+    // else{
+    //     //remove element from array
+    //     todos = _.without(todos, matchedTodo);
+    //     res.json(todos);
+    // }
 
 });
 
