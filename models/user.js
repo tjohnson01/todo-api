edf24e28c61189979f715226c8629f3542ed8aac
Travis Johnson <tjohnson01@gmail.com>
2015-10-04 07:32:38 -0600

    Salting and hashing pwd

--- models/user.js
+++ models/user.js
@@ -1,3 +1,6 @@
+var bcrypt = require('bcrypt');
+var _ = require('underscore');
+
 //format for sequelize.import
 
 module.exports = function(sequelize, DataTypes) {
@@ -10,11 +13,26 @@ module.exports = function(sequelize, DataTypes) {
                 isEmail: true
             }
         },
+        salt: {
+            type: DataTypes.STRING
+        },
+        password_hash:{
+            type: DataTypes.STRING
+        },
         password: {
-            type: DataTypes.STRING,
+            type: DataTypes.VIRTUAL,  //virtual not stored in db but accessible
             allowNull: false,
             validate: {
                 len: [7, 50]
+            },
+            set: function(value){
+                var salt = bcrypt.genSaltSync(10);
+                var hashedPassword = bcrypt.hashSync(value, salt);
+
+                this.setDataValue('password', value);
+                this.setDataValue('salt', salt);
+                this.setDataValue('password_hash', hashedPassword);
+
             }
         }
     }, {
@@ -25,6 +43,13 @@ module.exports = function(sequelize, DataTypes) {
                     user.email = user.email.toLowerCase();
                 }
             }
+        },
+        instanceMethods: {
+            toPublicJSON: function (){
+                var json = this.toJSON();  //this refers to instance
+                return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
+            }
+
         }
     });
 
