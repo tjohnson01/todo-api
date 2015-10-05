bef9d615901d169f68410c8522602ccf4c52a292
Travis Johnson <tjohnson01@gmail.com>
2015-10-05 10:17:30 -0600

    Add auth middleware and findByToken class method

--- models/user.js
+++ models/user.js
@@ -1,7 +1,9 @@
 var bcrypt = require('bcrypt');
 var _ = require('underscore');
+var cryptojs = require('crypto-js');
+var jwt = require('jsonwebtoken');
 
-module.exports = function (sequelize, DataTypes) {
+module.exports = function(sequelize, DataTypes) {
     var user = sequelize.define('user', {
         email: {
             type: DataTypes.STRING,
@@ -23,7 +25,7 @@ module.exports = function (sequelize, DataTypes) {
             validate: {
                 len: [7, 100]
             },
-            set: function (value) { 
+            set: function(value) {
                 var salt = bcrypt.genSaltSync(10);
                 var hashedPassword = bcrypt.hashSync(value, salt);
 
@@ -34,7 +36,7 @@ module.exports = function (sequelize, DataTypes) {
         }
     }, {
         hooks: {
-            beforeValidate: function (user, options) {
+            beforeValidate: function(user, options) {
                 // user.email
                 if (typeof user.email === 'string') {
                     user.email = user.email.toLowerCase();
@@ -42,8 +44,8 @@ module.exports = function (sequelize, DataTypes) {
             }
         },
         classMethods: {
-            authenticate: function(){
-                return new Promise(function(resolve, reject){
+            authenticate: function(body) {
+                return new Promise(function(resolve, reject) {
                     if (typeof body.email !== 'string' || typeof body.password !== 'string') {
                         return reject();
                     }
@@ -54,20 +56,60 @@ module.exports = function (sequelize, DataTypes) {
                         }
                     }).then(function(user) {
                         if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
-                            return reject(); //user not found (401 = authentication possible but not failed)
+                            return reject();
                         }
 
                         resolve(user);
-                    }, function(err) {
+                    }, function(e) {
                         reject();
                     });
-            });
+                });
+            },
+            findByToken: function(token){
+                return new Promise(function(resolve, reject){
+                    try{
+                        var decodedJWT = jwt.verify(token, 'qwerty098');
+                        var bytes = cryptojs.AES.decrypt(decodedJWT.token, 'abc123!@#!');
+                        var tokenData = JSON.parse(bytes.toString(cryptojs.enc.Utf8));
+
+                        user.findById(tokenData.id).then(function(user){
+                            if(user){
+                                resolve(user);
+                            } else{
+                                reject();
+                            }
+                        }, function(e){
+                            reject();
+                        });
+
+                    } catch(e){
+                        reject();
+                    }
+                });
             }
         },
         instanceMethods: {
-            toPublicJSON: function () {
+            toPublicJSON: function() {
                 var json = this.toJSON();
                 return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
+            },
+            generateToken: function (type) {
+                if (!_.isString(type)) {
+                    return undefined;
+                }
+
+                try {
+                    var stringData = JSON.stringify({id: this.get('id'), type: type});
+                    var encryptedData = cryptojs.AES.encrypt(stringData, 'abc123!@#!').toString();
+                    var token = jwt.sign({
+                        token: encryptedData
+                    }, 'qwerty098');
+
+                    return token;
+                } catch (e) {
+                    console.error(e);
+                    return undefined;
+                }
             }
         }
     });
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
 var bcrypt = require('bcrypt');
-
+var middleware = require('./middleware.js')(db);
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -17,7 +17,7 @@ app.get('/', function(req, res) {
 });
 
 // GET /todos?completed=false&q=work
-app.get('/todos', function(req, res) {
+app.get('/todos', middleware.requireAuthentication, function(req, res) {
     var query = req.query;
     var where = {};
 
@@ -43,7 +43,7 @@ app.get('/todos', function(req, res) {
 });
 
 // GET /todos/:id
-app.get('/todos/:id', function(req, res) {
+app.get('/todos/:id', middleware.requireAuthentication, function(req, res) {
     var todoId = parseInt(req.params.id, 10);
 
     db.todo.findById(todoId).then(function(todo) {
@@ -58,7 +58,7 @@ app.get('/todos/:id', function(req, res) {
 });
 
 // POST /todos
-app.post('/todos', function(req, res) {
+app.post('/todos',middleware.requireAuthentication, function(req, res) {
     var body = _.pick(req.body, 'description', 'completed');
 
     db.todo.create(body).then(function(todo) {
@@ -69,7 +69,7 @@ app.post('/todos', function(req, res) {
 });
 
 // DELETE /todos/:id
-app.delete('/todos/:id', function(req, res) {
+app.delete('/todos/:id', middleware.requireAuthentication, function(req, res) {
     var todoId = parseInt(req.params.id, 10);
 
     db.todo.destroy({
@@ -90,7 +90,7 @@ app.delete('/todos/:id', function(req, res) {
 });
 
 // PUT /todos/:id
-app.put('/todos/:id', function(req, res) {
+app.put('/todos/:id', middleware.requireAuthentication, function(req, res) {
     var todoId = parseInt(req.params.id, 10);
     var body = _.pick(req.body, 'description', 'completed');
     var attributes = {};
@@ -118,34 +118,34 @@ app.put('/todos/:id', function(req, res) {
     });
 });
 
-/*  User routes */
-
-app.post('/users', function(req, res) {
+app.post('/users', function (req, res) {
     var body = _.pick(req.body, 'email', 'password');
 
-    db.user.create(body).then(function(user) {
+    db.user.create(body).then(function (user) {
         res.json(user.toPublicJSON());
-    }, function(e) {
+    }, function (e) {
         res.status(400).json(e);
     });
 });
 
 // POST /users/login
-app.post('/users/login', function(req, res){
+app.post('/users/login', function (req, res) {
     var body = _.pick(req.body, 'email', 'password');
 
-    db.user.authenticate(body).then(function (user){
-        res.json(user.toPublicJSON());
-    }, function (){
-        res.status(401).send();  //vague unable to authenticate message
-    });
+    db.user.authenticate(body).then(function (user) {
+        var token = user.generateToken('authentication');
 
+        if (token) {
+            res.header('Auth', token).json(user.toPublicJSON());    
+        } else {
+            res.status(401).send();
+        }
+    }, function () {
+        res.status(401).send();
+    });
 });
 
-
-
-
-db.sequelize.sync({force:true }).then(function() {
+db.sequelize.sync({force: true}).then(function() {
     app.listen(PORT, function() {
         console.log('Express listening on port ' + PORT + '!');
     });
